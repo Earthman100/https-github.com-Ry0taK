223a9abf2808fb3cc45786c1be480544cab03351
Ry0taK <49341894+Ry0taK@users.noreply.github.com>
2023-02-11 13:15:47 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Misskey CSP Logger
+misskey.ioにおいてCSP関連のエラーを記録するためのパッケージ
+
+**注意:** `/csp-error`へPOSTされた内容をそのまま`console.log`に書き出すだけのものです
---
+++ index.js
@@ -0,0 +1,14 @@
+const fastify = require('fastify')();
+
+fastify.addContentTypeParser('application/csp-report', { parseAs: 'string' }, fastify.getDefaultJsonParser('ignore', 'ignore'));
+
+fastify.post('/csp-error', async (request, reply) => {
+    console.log(JSON.stringify(request.body));
+    reply.code(204);
+    return {};
+});
+
+fastify.listen({ port: 3000 }, (err, address) => {
+    if (err) throw err;
+    console.log(`Server is listening on ${address}`);
+});
